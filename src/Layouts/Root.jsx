2af84a58a4a176d7728990edecc254217f10c76c
Milton Ahmed <miltonahmed08@yahoo.com>
2025-10-19 20:28:23 +0400

    aside both are make stiky

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -10,13 +10,13 @@ const Root = () => {
         <Header />
       </header>
       <main className='max-w-11/12 mx-auto grid grid-cols-12'>
-        <aside className='col-span-3'>
+        <aside className='col-span-3 sticky top-0 h-fit'>
           <LeftAside></LeftAside>
         </aside>
         <section className='col-span-6'>
           <Outlet />
         </section>
-        <aside className='col-span-3'>
+        <aside className='col-span-3 sticky top-0 h-fit'>
           <RightAside></RightAside>
         </aside>
       </main>
